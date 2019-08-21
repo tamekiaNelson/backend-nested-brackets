b4ee0e37b4b68cecede7dca15054347d0ea128d2
Tamekia N <tamekialashay@outlook.com>
2019-08-21 13:25:19 -0400

    submit assignment to check nested brakets

--- nested.py
+++ nested.py
@@ -1,15 +1,40 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Module docstring: One line description of what your program does.
+Module docstring: checks for proper brackets.
 """
-__author__ = "Your Github Username"
+__tamekiaNelson__ = "Your Github Username"
 
 import sys
 
+open_li = ["[","(","{","(*"]
+closed_li = ["]",")","}","*)"," "]
+
+
+def valid_parentheses(string):
+    stack = []
+    for i in string:
+        if i in open_li:
+            stack.append(i)
+        elif i in closed_li:
+            x = closed_li.index(i)
+            if ((len(stack) > 0) and (open_li[x] == stack[len(stack) -1])):
+                stack.pop()
+            else:
+                return False
+    if len(stack) == 0:
+        return True
+    else:
+        return False
+
 def main(args):
-    """Add your code here"""
+    with open("input.txt", "a") as file_open:
+      with open("output.txt", "r") as output:
+        for row in file_open:
+              results = valid_parentheses(row)
+              print(str(results) + "\n")
     
 
 if __name__ == '__main__':
     main(sys.argv)
+
