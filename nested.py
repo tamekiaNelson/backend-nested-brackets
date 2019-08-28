3aae408036a8dd5579ed41e1271e9c4ea1348f0b
Tamekia N <tamekialashay@outlook.com>
2019-08-28 19:15:35 -0400

    corrected code

--- nested.py
+++ nested.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-Module docstring: checks for proper brackets.
-"""
-__tamekiaNelson__ = "Your Github Username"
+""" Module docstring: checks for proper brackets. """
+__name__ = "tamekiaNelson"
 
 import sys
 
@@ -21,20 +19,19 @@ def valid_parentheses(string):
             if ((len(stack) > 0) and (open_li[x] == stack[len(stack) -1])):
                 stack.pop()
             else:
-                return False
+                return "No"
     if len(stack) == 0:
-        return True
+        return "Yes"
     else:
-        return False
+        return "No"
 
-def main(args):
-    with open("input.txt", "a") as file_open:
-      with open("output.txt", "r") as output:
-        for row in file_open:
-              results = valid_parentheses(row)
-              print(str(results) + "\n")
-    
+def main():
+    with open("input.txt", "r") as file_open:
+        with open("output.txt", "w") as output:
+            for row in file_open:
+                results = valid_parentheses(row)
+                print(results)
+                output.write(results + "\n")
 
 if __name__ == '__main__':
-    main(sys.argv)
-
+    main()
