fb0930efcb22ecb1acc7fa2f455ef0155449b567
Tamekia N <tamekialashay@outlook.com>
2019-08-29 16:09:07 -0400

    I am submitting another final solution to replace the other final solution

--- nested.py
+++ nested.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """ Module docstring: checks for proper brackets. """
-__name__ = "tamekiaNelson"
+__author__ = "tamekiaNelson"
 
 import sys
 
 open_li = ["[","(","{","(*"]
-closed_li = ["]",")","}","*)"," "]
+closed_li = ["]",")","}","*)",""]
 
 
 def valid_parentheses(string):
